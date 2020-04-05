67bf5a78de7a6d61e7d89c8b41c7d95a1e93637c
vladpetrovskyi <v.petrovskyi98@gmail.com>
2020-04-05 13:43:43 +0300

    finalized the program.

--- src/main/java/core/basesyntax/MainApp.java
+++ src/main/java/core/basesyntax/MainApp.java
@@ -1,44 +1,98 @@
 package core.basesyntax;
 
+import java.io.IOException;
 import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 import static core.basesyntax.Methods.*;
 
 public class MainApp {
 
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        System.out.print("Write your request in format 'command [path] [filename]': ");
-        String request = scanner.nextLine();
+        Scanner scanner = null;
+        boolean finalDecision = true;
+        do {
+            String request = requestStringReturner();
 
-        String[] requestArray = request.split(" ");
-        String command = requestArray[0].toLowerCase();
-        String path = (requestArray.length == 2) ?
-                requestArray[1].substring(1, requestArray[1].length() - 1) : null;
-        String filename = (requestArray.length == 3) ?
-                requestArray[2].toLowerCase().substring(1, requestArray[2].length() - 1) : null;
+            Pattern pattern2Word = Pattern.compile("^\\w{4}\\s\\[\\w{4,6}]$");
+            Pattern pattern3Word = Pattern.compile("^\\w{4,6}\\s\\[.+]\\s\\[.+]$");
+            Matcher matcher2Word = pattern2Word.matcher(request);
+            Matcher matcher3Word = pattern3Word.matcher(request);
 
-        switch (command) {
-            case "help":
-                if (requestArray.length > 1) {
-                    helpByCommand(path);
-                } else {
-                    help();
+            if (matcher3Word.matches()) {
+                String[] requestArray = request.split(" ");
+                String command = requestArray[0].toLowerCase();
+                String path = requestArray[1].substring(1, requestArray[1].length() - 1);
+                String filename = requestArray[2].substring(1, requestArray[2].length() - 1);
+
+                switch (command) {
+                    case "create":
+                        createFile(path, filename);
+                        break;
+                    case "read":
+                        try {
+                            readFile(path, filename);
+                        } catch (IOException e) {
+                            System.out.println("No file/directory found!");
+                        }
+                        break;
+                    case "info":
+                        try {
+                            fileInfo(path, filename);
+                        } catch (IOException e) {
+                            System.out.println("No file/directory found!");
+                        }
+                        break;
+                    default:
+                        saveText(command);
                 }
-                break;
-            case "create":
-                createFile(path, filename);
-                break;
-            case "read":
-                readFile(path, filename);
-                break;
-            case "info":
-                fileInfo(path, filename);
-                break;
-            case "exit":
-                break;
-            default:
-                saveText();
+            } else if (matcher2Word.matches() && request.split(" ")[0]
+                    .toLowerCase().contentEquals("help")) {
+                helpByCommand(request.split(" ")[1]
+                        .substring(1, request.split(" ")[1].length() - 1));
+            } else {
+                switch (request) {
+                    case "help":
+                        help();
+                        break;
+                    case "exit":
+                        finalDecision = false;
+                        break;
+                    default:
+                        saveText(request);
+                }
             }
+
+            if (!request.toLowerCase().contentEquals("exit")) {
+                boolean localDecision = true;
+                do {
+                    scanner = new Scanner(System.in);
+                    System.out.print("\nWould you like to do anything else? (y/n): ");
+                    String answer = scanner.nextLine();
+                    if (answer.contentEquals("n")) {
+                        localDecision = false;
+                        finalDecision = false;
+                    } else if (answer.contentEquals("y")) {
+                        localDecision = false;
+                    } else {
+                        System.out.println("Wrong input, try again!");
+                    }
+                } while (localDecision);
+            }
+        } while (finalDecision);
+
+        System.out.println("Program is ending...");
+        if (scanner != null) {
+            scanner.close();
         }
     }
+
+    public static String requestStringReturner() {
+        Scanner scanner;
+        scanner = new Scanner(System.in);
+        System.out.print("\nWrite your request in next format 'command [/path/.../] [filename.extension]' " +
+                "or write 'help' to see all available commands: ");
+        return scanner.nextLine();
+    }
+}
--- src/main/java/core/basesyntax/Methods.java
+++ src/main/java/core/basesyntax/Methods.java
@@ -1,30 +1,47 @@
 package core.basesyntax;
 
-import java.io.File;
+import java.io.FileWriter;
 import java.io.IOException;
+import java.io.Writer;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.nio.file.attribute.FileTime;
+import java.text.SimpleDateFormat;
+import java.util.List;
 import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class Methods {
 
-    protected static void createFile(String path, String filename) {
-        Path paths = Paths.get(path + File.separator + filename);
-        if (Files.exists(paths)) {
-            System.out.println("File already exists. Would you like to rewrite it? (y/n): ");
-            Scanner scanner = new Scanner(System.in);
-            String result = scanner.nextLine();
-//            if(result.equals("y")) {
-//
-//            } else if (result.equals("n")) {
-//
-//            } else {
-//                System.out.println("Wrong input!");
-//            }
+    private static Scanner scanner;
+
+    protected static void createFile(String pathURI, String filename) {
+        Path path = Paths.get(pathURI);
+        Path file = Paths.get(pathURI, filename);
+        if (Files.exists(file)) {
+            boolean localDecision = true;
+            do {
+                System.out.print("File already exists. Would you like to overwrite it? (y/n): ");
+                scanner = new Scanner(System.in);
+                String answer = scanner.nextLine();
+                if (answer.contentEquals("n")) {
+                    localDecision = false;
+                } else if (answer.contentEquals("y")) {
+                    try (Writer fileWriter = new FileWriter(String.valueOf(file), false)) {
+                        System.out.println("File was overwritten.");
+                    } catch (IOException e) {
+                        System.out.println("Path does not exist!");
+                    }
+                    localDecision = false;
+                } else {
+                    System.out.println("Wrong input, try again!");
+                }
+            } while (localDecision);
         } else {
             try {
-                Files.createFile(paths);
+                Files.createFile(file);
             } catch (IOException e) {
                 System.out.println("Path does not exist!");
             }
@@ -32,25 +49,132 @@ public class Methods {
         }
     }
 
-    protected static void readFile(String path, String filename) {
-        String result = "";
-        System.out.println(result);
+    protected static void readFile(String path, String filename) throws IOException {
+        List<String> result = Files.readAllLines(Paths.get(path, filename));
+        for (String s : result) {
+            System.out.println(s);
+        }
     }
 
-    protected static void fileInfo(String path, String filename) {
-        System.out.println();
+    protected static void fileInfo(String path, String filename) throws IOException {
+        List<String> strings = Files.readAllLines(Paths.get(path, filename));
+        int symbols = 0;
+        int words = 0;
+        String[] wordsArray;
+        for (String s : strings) {
+            wordsArray = s.split(" ");
+            words += wordsArray.length;
+            for (String s1 : wordsArray) {
+                symbols += s1.length();
+            }
+        }
+        long changed = Files.getLastModifiedTime(Paths.get(path, filename)).toMillis();
+        String pattern = "E, dd MMM yyyy HH:mm:ss z";
+        SimpleDateFormat date = new SimpleDateFormat(pattern);
+        long size = Files.size(Paths.get(path, filename));
+        System.out.println("Symbols quantity: " + symbols + " symbols;"
+                + "\nStrings quantity: " + strings.size() + " strings;"
+                + "\nWords quantity: " + words + " words;"
+                + "\nLast changed: " + date.format(changed) + ";"
+                + "\nFile size: " + size + " bytes.");
     }
 
     protected static void help() {
-
+        System.out.println("create [path] [file-name]\n" +
+                "Создает текстовый файл по указанному пути. " +
+                "Если путь не существует, вывести соответствующее сообщение. " +
+                "Если файл уже существует, вывести запрос на его перезапись\n" +
+                "\n" +
+                "read [path] [file-name]\n" +
+                "Считывает файл по указанному пути и выводит текст в консоль. " +
+                "Если указанного пути и/или файла не существует, " +
+                "вывести соответствующее сообщение\n" +
+                "\n" +
+                "info [path] [file-name]\n" +
+                "Выводит краткую информацию по указанному файлу: " +
+                "количество символов, строк, слов, " +
+                "дату и время последнего изменения, размер файла\n" +
+                "\n" +
+                "help\n" +
+                "Выводит в консоль все доступные комманды и информацию к ним\n" +
+                "\n" +
+                "help [command]\n" +
+                "Выводит в консоль информацию по указанной команде\n" +
+                "\n" +
+                "exit\n" +
+                "Завершение работы программы");
     }
 
     protected static void helpByCommand(String command) {
-
+        switch (command) {
+            case "help":
+                System.out.println("help [command]\n" +
+                        "Выводит в консоль все доступные комманды и информацию к ним\n");
+                break;
+            case "create":
+                System.out.println("create [path] [file-name]\n" +
+                        "Создает текстовый файл по указанному пути. " +
+                        "Если путь не существует, вывести соответствующее сообщение. " +
+                        "Если файл уже существует, вывести запрос на его перезапись\n");
+                break;
+            case "read":
+                System.out.println("read [path] [file-name]\n" +
+                        "Считывает файл по указанному пути и выводит текст в консоль. " +
+                        "Если указанного пути и/или файла не существует, " +
+                        "вывести соответствующее сообщение\n");
+                break;
+            case "info":
+                System.out.println("info [path] [file-name]\n" +
+                        "Выводит краткую информацию по указанному файлу: " +
+                        "количество символов, строк, слов, " +
+                        "дату и время последнего изменения, размер файла\n");
+                break;
+            case "exit":
+                System.out.println("exit\n" +
+                        "Завершение работы программы");
+                break;
+            default:
+                System.out.println("Wrong command!");
+        }
     }
 
-    protected static void saveText() {
-        System.out.println("Would you like to save your text? (y/n): ");
-
+    protected static void saveText(String string) {
+        boolean decision = true;
+        do {
+            scanner = new Scanner(System.in);
+            System.out.print("No command found. Would you like to save your text? (y/n): ");
+            String answer = scanner.nextLine();
+            if (answer.contentEquals("y")) {
+                scanner = new Scanner(System.in);
+                System.out.print("Please indicate the name and path for a new file " +
+                        "in the next format [path] [name]: ");
+                Pattern pattern2Word = Pattern.compile("^\\[.+]\\s\\[.+]$");
+                String pathAndName = scanner.nextLine();
+                Matcher matcher2Word = pattern2Word.matcher(pathAndName);
+                boolean localDecision = true;
+                do {
+                    if (matcher2Word.matches()) {
+                        String[] answerArray = pathAndName.split(" ");
+                        String path = answerArray[0].substring(1, answerArray[0].length() - 1);
+                        String name = answerArray[1].substring(1, answerArray[1].length() - 1);
+                        createFile(path, name);
+                        try {
+                            Files.write(Paths.get(path, name), string.getBytes());
+                            System.out.print("Your text has been saved!");
+                        } catch (IOException e) {
+                            throw new RuntimeException("File could not be created!");
+                        }
+                        decision = false;
+                        localDecision = false;
+                    } else {
+                        System.out.println("Wrong input, try again!");
+                    }
+                } while (localDecision);
+            } else if (answer.contentEquals("n")) {
+                decision = false;
+            } else {
+                System.out.println("Wrong input, try again!");
+            }
+        } while (decision);
     }
 }
