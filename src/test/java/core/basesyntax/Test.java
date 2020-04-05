ed4b50332b7b0b7f972124f465e7156dcd1d5edb
6ALLIKA <skorik009@gmail.com>
2020-04-05 20:27:54 +0300

    beta version of FileEditor

---
+++ src/test/java/core/basesyntax/Test.java
@@ -0,0 +1,10 @@
+package core.basesyntax;
+
+import java.io.IOException;
+public class Test {
+
+    public static void main(String[] args) throws IOException {
+        FileEditor fileEditor = new FileEditor();
+        fileEditor.start();
+    }
+}
