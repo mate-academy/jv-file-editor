74248bff727dcb6bc1e17571b82679133d5a141d
Oleh Kuzmenko <oleg24453@gmail.com>
2020-04-05 15:56:41 +0300

    writed console aplication

---
+++ src/main/java/core/basesyntax/FileEditor.java
@@ -0,0 +1,48 @@
+package core.basesyntax;
+
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.Formatter;
+import java.util.List;
+import java.util.Scanner;
+
+public class FileEditor {
+    public static void main(String[] args) throws IOException {
+        System.out.println("Введіть вашу команду");
+        Scanner comand = new Scanner(System.in);
+        String textOfComand = comand.nextLine();
+        if(textOfComand.contains("create ")) {
+            Creating.arguments(textOfComand);
+        }
+        else if(textOfComand.contains("read ")) {
+            Reading.arguments(textOfComand);
+        }
+        else if(textOfComand.contains("info ")) {
+            ShowInfo.arguments(textOfComand);
+        }
+        else if(textOfComand.contains("help")) {
+            Helping.comands(textOfComand);
+        }
+        else if(textOfComand.equals("exit")) {
+            System.exit(0);
+        }
+        else {
+            String text = textOfComand;
+            System.out.println("Бажаєте зберегти цей текст?");
+            Scanner enter = new Scanner(System.in);
+            if (enter.nextLine().equals("так")) {
+                try (FileWriter fileWriter = new FileWriter(new File("newFile.txt"))) {
+                    fileWriter.write(text);
+                } catch (IOException e) {
+                    throw new RuntimeException("Error while writing text to the file");
+                }
+            }
+            else {
+                System.exit(0);
+            }
+        }
+    }
+}
---
+++ src/main/java/core/basesyntax/Helping.java
@@ -0,0 +1,36 @@
+package core.basesyntax;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.List;
+
+public class Helping {
+
+    public static void comands(String comad) throws IOException {
+        List<String> lines = Files.readAllLines(Paths.get("help.txt"));
+        if(comad.equals("help")) {
+            for (String s : lines) {
+                System.out.println(s);
+            }
+        }
+        if(comad.equals("help [create]")) {
+            System.out.println(lines.get(0));
+        }
+        else if(comad.equals("help [read]")) {
+            System.out.println(lines.get(1));
+        }
+        else if(comad.equals("help [info]")) {
+            System.out.println(lines.get(2));
+        }
+        else if(comad.equals("help [help]")) {
+            System.out.println(lines.get(3));
+        }
+        else if(comad.equals("help [command]")) {
+            System.out.println(lines.get(4));
+        }
+        else if(comad.equals("help [exit]")) {
+            System.out.println(lines.get(5));
+        }
+    }
+}
