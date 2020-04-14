ce9e8753ab2ea572a18269c755c33a1658bbb412
My <vasylnv19@gmail.com>
2020-04-14 19:37:02 +0300

    added change

--- src/main/java/core/basesyntax/Application.java
+++ src/main/java/core/basesyntax/Application.java
@@ -4,11 +4,10 @@ import java.io.IOException;
 import java.util.Map;
 import java.util.Scanner;
 
-import static core.basesyntax.GeneratMenu.generatMenu;
-
 public class Application {
     public static void main(String[] args) throws IOException {
-        Map<Integer, String> map = generatMenu();
+        MenuController menuController = new MenuController();
+        Map<Integer, String> map = menuController.generatMenu();
         Scanner scan = new Scanner(System.in);
         boolean isExitChosen = false;
         while (!isExitChosen) {
@@ -17,9 +16,8 @@ public class Application {
             for (Map.Entry<Integer, String> entry : map.entrySet()) {
                 System.out.println(entry.getKey() + entry.getValue());
             }
-            //readerList();
             chosenItem = scan.nextLine();
-            isExitChosen = MenuController.processUserSelection(chosenItem);
+            isExitChosen = menuController.processUserSelection(chosenItem);
 
         }
 
--- src/main/java/core/basesyntax/MenuController.java
+++ src/main/java/core/basesyntax/MenuController.java
@@ -1,40 +1,36 @@
 package core.basesyntax;
 
-
 import java.io.IOException;
-import java.text.SimpleDateFormat;
-import java.util.Date;
-import java.util.Scanner;
-
-import static core.basesyntax.CreateFile.createFile;
-import static core.basesyntax.Info.getInfoAboutFile;
-import static core.basesyntax.ReadFile.readFiles;
+import java.util.HashMap;
+import java.util.Map;
 
 public class MenuController {
 
-    private static String obtainCurrentDateString() {
-        String pattern = "dd.MM";
-        SimpleDateFormat simpleDateFormat = new SimpleDateFormat(pattern);
-        String date = simpleDateFormat.format(new Date());
-        return date;
+    public Map<Integer, String> generatMenu() {
+        Map<Integer, String> mapMenu = new HashMap<>();
+        mapMenu.put(1, " --Create file");
+        mapMenu.put(2, " --Read file");
+        mapMenu.put(3, " --Info about file");
+        mapMenu.put(4, " --HelpCommand");
+        mapMenu.put(5, " --Exit");
+        return mapMenu;
     }
 
-    public static String todayDate = obtainCurrentDateString();
-
-    public static boolean processUserSelection(String chosenItem) throws IOException {
+    public boolean processUserSelection(String chosenItem) throws IOException {
+        FileEditor fileEditor = new FileEditor();
         boolean isExitChosen = false;
         switch (chosenItem) {
             case "1":
                 System.out.println("You chose: Create new files");
-                createFile();
+                fileEditor.createFile();
                 break;
             case "2":
                 System.out.println("You chose: Read file");
-                readFiles();
+                fileEditor.readFiles();
                 break;
             case "3":
                 System.out.println("You chose: Info about file");
-                getInfoAboutFile();
+                fileEditor.getInfoAboutFile();
                 break;
             case "4":
                 System.out.println("You chose: helpCommand");
