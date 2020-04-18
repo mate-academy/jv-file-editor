20c8cb846dc3c4b8bb20d88365b253397d8d0b90
Oleh Kuzmenko <oleg24453@gmail.com>
2020-04-18 21:46:37 +0300

    added correction

--- src/main/java/core/basesyntax/FileEditor.java
+++ src/main/java/core/basesyntax/FileEditor.java
@@ -1,38 +1,10 @@
 package core.basesyntax;
 
-import java.io.File;
-import java.io.FileWriter;
 import java.io.IOException;
-import java.util.Scanner;
 
 public class FileEditor {
     public static void main(String[] args) throws IOException {
-        System.out.println("Введіть вашу команду");
-        Scanner comand = new Scanner(System.in);
-        String textOfComand = comand.nextLine();
-        if (textOfComand.contains("create ")) {
-            Creating.arguments(textOfComand);
-        } else if (textOfComand.contains("read ")) {
-            Reading.arguments(textOfComand);
-        } else if (textOfComand.contains("info ")) {
-            ShowInfo.arguments(textOfComand);
-        } else if (textOfComand.contains("help")) {
-            Helping.comands(textOfComand);
-        } else if (textOfComand.equals("exit")) {
-            System.exit(0);
-        } else {
-            String text = textOfComand;
-            System.out.println("Бажаєте зберегти цей текст?");
-            Scanner enter = new Scanner(System.in);
-            if (enter.nextLine().equals("так")) {
-                try (FileWriter fileWriter = new FileWriter(new File("newFile.txt"))) {
-                    fileWriter.write(text);
-                } catch (IOException e) {
-                    throw new RuntimeException("Error while writing text to the file");
-                }
-            } else {
-                System.exit(0);
-            }
-        }
+        Command command = new Command();
+        command.fileReader();
     }
 }
