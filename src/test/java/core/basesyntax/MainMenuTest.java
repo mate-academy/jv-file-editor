7f63f274831ef8ba06655264ceee1e6ddd9b9a77
Galina <gala6ap@mail.ru>
2020-04-20 15:52:26 +0300

    Refactoring. Provided test cases independence

--- src/test/java/core/basesyntax/MainMenuTest.java
+++ src/test/java/core/basesyntax/MainMenuTest.java
@@ -15,7 +15,7 @@ public class MainMenuTest {
     private static final String TEST_FILE_NAME = "testFileName";;
 
     @Test
-    public void mainMenuTest() {
+    public void mainMenuInfoCommandTest() {
         Queue<String> queue = new LinkedList<>();
         queue.add("info " + TEST_PATH + " " + TEST_FILE_NAME);
         queue.add("exit");
@@ -28,37 +28,87 @@ public class MainMenuTest {
         menu.start();
         Assert.assertTrue("INFO command invoke fail",
                 fileHelper.isCommandInvoked(Command.INFO, List.of(TEST_PATH, TEST_FILE_NAME)));
+    }
 
+    @Test
+    public void mainMenuReadCommandTest() {
+        Queue<String> queue = new LinkedList<>();
         queue.add("read " + TEST_PATH + " " + TEST_FILE_NAME);
         queue.add("exit");
+
+        MockConsoleHelper consoleHelper = new MockConsoleHelper(queue);
+        MockFileHelper fileHelper = new MockFileHelper();
+
+        MainMenu menu = new MainMenu(new MockHelperFactory(consoleHelper, fileHelper));
+
         menu.start();
         Assert.assertTrue("READ command invoke fail",
                 fileHelper.isCommandInvoked(Command.READ, List.of(TEST_PATH, TEST_FILE_NAME)));
+    }
 
+    @Test
+    public void mainMenuCreateCommandTest() {
+        Queue<String> queue = new LinkedList<>();
         queue.add("create " + TEST_PATH + " " + TEST_FILE_NAME);
         queue.add("exit");
+
+        MockConsoleHelper consoleHelper = new MockConsoleHelper(queue);
+        MockFileHelper fileHelper = new MockFileHelper();
+
+        MainMenu menu = new MainMenu(new MockHelperFactory(consoleHelper, fileHelper));
+
         menu.start();
         Assert.assertTrue("CREATE command invoke fail",
                 fileHelper.isCommandInvoked(Command.CREATE, List.of(TEST_PATH, TEST_FILE_NAME, "")));
+    }
 
+    @Test
+    public void mainMenuSaveTextToFileCommandTest() {
+        Queue<String> queue = new LinkedList<>();
         String fileContent = "some text";
         queue.add(fileContent);
         queue.add("y");
         queue.add(TEST_PATH);
         queue.add(TEST_FILE_NAME);
         queue.add("exit");
+
+        MockConsoleHelper consoleHelper = new MockConsoleHelper(queue);
+        MockFileHelper fileHelper = new MockFileHelper();
+
+        MainMenu menu = new MainMenu(new MockHelperFactory(consoleHelper, fileHelper));
+
         menu.start();
         Assert.assertTrue("Save text to file command invoke fail",
                 fileHelper.isCommandInvoked(Command.CREATE, List.of(TEST_PATH, TEST_FILE_NAME, fileContent)));
+    }
 
+    @Test
+    public void mainMenuHelpCommandTest() {
+        Queue<String> queue = new LinkedList<>();
         queue.add("help");
         queue.add("exit");
+
+        MockConsoleHelper consoleHelper = new MockConsoleHelper(queue);
+        MockFileHelper fileHelper = new MockFileHelper();
+
+        MainMenu menu = new MainMenu(new MockHelperFactory(consoleHelper, fileHelper));
+
         menu.start();
         Assert.assertTrue("HELP command invoke fail",
                 fileHelper.isCommandInvoked(Command.HELP, List.of()));
+    }
 
+    @Test
+    public void mainMenuHelpCommandWithParamsTest() {
+        Queue<String> queue = new LinkedList<>();
         queue.add("help " + Command.CREATE);
         queue.add("exit");
+
+        MockConsoleHelper consoleHelper = new MockConsoleHelper(queue);
+        MockFileHelper fileHelper = new MockFileHelper();
+
+        MainMenu menu = new MainMenu(new MockHelperFactory(consoleHelper, fileHelper));
+
         menu.start();
         Assert.assertTrue("HELP command with params invoke fail",
                 fileHelper.isCommandInvoked(Command.HELP, List.of(Command.CREATE.toString())));
