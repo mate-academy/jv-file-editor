29ed60d6fdd4990597a302a50fed99f917b81ff1
Vlad Kuzmin <vladkuzmin22@gmail.com>
2020-08-02 15:28:40 +0300

    fix style

--- src/main/java/core/basesyntax/Main.java
+++ src/main/java/core/basesyntax/Main.java
@@ -1,20 +1,31 @@
 package core.basesyntax;
 
-import java.io.*;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
 import java.util.Scanner;
 
 public class Main {
 
-    public static final String INFO_CREATE = "create [path] [file-name] - Создает текстовый файл по указанному пути;";
-    public static final String INFO_READ = "read [path] [file-name] - Считывает файл по указанному пути и выводит текст в консоль;";
-    public static final String INFO_INFO = "info [path] [file-name] - Выводит краткую информацию по указанному файлу;";
-    public static final String INFO_HELP = "help - Выводит в консоль все доступные команды и информацию к ним;";
-    public static final String INFO_HELP_COMMAND = "help [command] - Выводит в консоль информацию по указанной команде;";
-    public static final String INFO_EXIT = "exit - Завершение работы программы.";
+    public static final String INFO_CREATE
+            = "create [path] [file-name] - Создает текстовый файл по указанному пути;";
+    public static final String INFO_READ
+            = "read [path] [file-name] - Считывает файл по указанному пути и "
+            + "выводит текст в консоль;";
+    public static final String INFO_INFO
+            = "info [path] [file-name] - Выводит краткую информацию по указанному файлу;";
+    public static final String INFO_HELP
+            = "help - Выводит в консоль все доступные команды и информацию к ним;";
+    public static final String INFO_HELP_COMMAND
+            = "help [command] - Выводит в консоль информацию по указанной команде;";
+    public static final String INFO_EXIT
+            = "exit - Завершение работы программы.";
 
-    public static void chooseCommand (String input) {
+    public static void chooseCommand(String input) {
         String[] command = input.split(" ");
         switch (command[0]) {
             case "create":
@@ -68,7 +79,7 @@ public class Main {
     public static String read(File file) {
         String read = "";
         try (FileReader fileReader = new FileReader(file);
-             Scanner readScanner = new Scanner(fileReader)) {
+                Scanner readScanner = new Scanner(fileReader)) {
             while (readScanner.hasNextLine()) {
                 read += readScanner.nextLine() + "\n";
             }
@@ -85,7 +96,7 @@ public class Main {
         String read = "";
         DateFormat dateFormat = new SimpleDateFormat("dd/MM/yyyy - hh:mm:ss");
         try (FileReader fileReader = new FileReader(file);
-             Scanner readScanner = new Scanner(fileReader)) {
+                Scanner readScanner = new Scanner(fileReader)) {
             while (readScanner.hasNextLine()) {
                 read += readScanner.nextLine() + "\n";
             }
@@ -93,7 +104,8 @@ public class Main {
                     .append("Количество символов: ").append(read.split("").length).append("\n")
                     .append("Количество слов: ").append(read.split(" ").length).append("\n")
                     .append("Количество строк: ").append(read.split("\n").length).append("\n")
-                    .append("Дата и время последнего изменения: ").append(dateFormat.format(file.lastModified())).append("\n")
+                    .append("Дата и время последнего изменения: ")
+                    .append(dateFormat.format(file.lastModified())).append("\n")
                     .append("Размер файла ").append(file.length()).append(" bytes");
             System.out.println(info);
         } catch (FileNotFoundException e) {
@@ -138,17 +150,18 @@ public class Main {
         }
     }
 
-    public static void noCommand (String someText) {
+    public static void noCommand(String someText) {
         Scanner scanner = new Scanner(System.in);
         System.out.println("Хотите записать данный текст в файл?  Y - Да, N - Нет");
         switch (new Scanner(System.in).nextLine().toUpperCase()) {
             case "Y":
                 String text = someText;
-                System.out.println("Укажите в какой файл делать запись в формате [путь] [имя_файла]: ");
+                System.out.println("Укажите в какой файл делать запись в "
+                        + "формате [путь] [имя_файла]: ");
                 String input = scanner.nextLine();
                 String[] command = input.split(" ");
                 File file = new File(command[0], command[1]);
-                if(file.exists()) {
+                if (file.exists()) {
                     write(file, someText);
                 } else {
                     System.out.println("Данного файла по заданному пути не существует");
