acb1bbdcac7112dd323f9e1c457f61148eb61e23
Borisko130 <krimenyukb@gmail.com>
2020-08-18 21:05:28 +0300

    Moved different functionality to different classes

--- src/main/java/core/basesyntax/FileEditor.java
+++ src/main/java/core/basesyntax/FileEditor.java
@@ -1,16 +1,12 @@
 package core.basesyntax;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.nio.file.FileSystems;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
 import java.util.HashMap;
 import java.util.Scanner;
 
 public class FileEditor {
     private Scanner scanner = new Scanner(System.in);
+    private FileCreator fileCreator = new FileCreator();
+    private FileReader fileReader = new FileReader();
 
     public void start() {
         boolean work = true;
@@ -23,19 +19,19 @@ public class FileEditor {
                     if (variant.length != 3) {
                         break;
                     }
-                    System.out.print(create(variant[1], variant[2]));
+                    System.out.print(fileCreator.create(variant[1], variant[2]));
                     break;
                 case "read":
                     if (variant.length != 3) {
                         break;
                     }
-                    System.out.print(read(variant[1], variant[2]));
+                    System.out.print(fileReader.read(variant[1], variant[2]));
                     break;
                 case "info":
                     if (variant.length != 3) {
                         break;
                     }
-                    System.out.print(info(variant[1], variant[2]));
+                    System.out.print(fileReader.info(variant[1], variant[2]));
                     break;
                 case "help":
                     if (variant.length == 2) {
@@ -62,66 +58,6 @@ public class FileEditor {
     private void infoMessage() {
         System.out.println("Please enter valid command with correct amount parameters.\n"
                 + "You can get list of available commands by entering 'help'.\n>");
-    }
-
-    private String create(String path, String filename) {
-        Path newFilePath = Paths.get(path + FileSystems.getDefault().getSeparator() + filename);
-        if (Files.exists(newFilePath)) {
-            throw new IllegalArgumentException("File already exists");
-        }
-        try {
-            Files.createFile(newFilePath);
-            return "File at " + path + FileSystems.getDefault().getSeparator()
-                    + filename + " was created\n>";
-        } catch (IOException e) {
-            throw new IllegalArgumentException("Path does not exist");
-        }
-    }
-
-    private String read(String path, String filename) {
-        StringBuilder buffer = new StringBuilder();
-        Path filePath = Paths.get(path + FileSystems.getDefault().getSeparator() + filename);
-        try (BufferedReader reader = Files.newBufferedReader(filePath)) {
-            String currentLine;
-            while ((currentLine = reader.readLine()) != null) {
-                buffer.append(currentLine).append('\n');
-            }
-        } catch (IOException e) {
-            throw new IllegalArgumentException("File not found");
-        }
-        return buffer.toString();
-    }
-
-    private String info(String path, String filename) {
-        int charNumber = 0;
-        int linesNumber = 0;
-        int wordsNumber = 0;
-        StringBuilder buffer = new StringBuilder();
-        Path filePath = Paths.get(path + FileSystems.getDefault().getSeparator() + filename);
-        try (BufferedReader reader = Files.newBufferedReader(filePath)) {
-            String currentLine;
-            while ((currentLine = reader.readLine()) != null) {
-                charNumber += currentLine.length();
-                wordsNumber += currentLine.split(" ").length;
-                linesNumber++;
-            }
-        } catch (IOException e) {
-            throw new IllegalArgumentException("File not found");
-        }
-        try {
-            buffer.append("File ").append(filePath)
-                    .append(" has following parameters:\n")
-                    .append("Amount of characters: ").append(charNumber)
-                    .append("\nNumber of words: ").append(wordsNumber)
-                    .append("\nNumber of lines: ").append(linesNumber).append("\nSize: ")
-                    .append(Files.getAttribute(filePath, "size").toString())
-                    .append(" bytes\n").append("Last modification time: ")
-                    .append(Files.getAttribute(filePath, "lastModifiedTime").toString())
-                    .append("\n");
-        } catch (IOException e) {
-            throw new IllegalArgumentException("Cannot read attribute");
-        }
-        return buffer.toString();
     }
 
     private void help() {
@@ -167,7 +103,7 @@ public class FileEditor {
                 System.out.println("Please enter path to file and filename separated with space\n");
                 String[] input = scanner.nextLine().split(" ");
                 if (input.length == 2) {
-                    create(input[0], input[1]);
+                    fileCreator.create(input[0], input[1]);
                     break;
                 }
                 System.out.println("Please enter path to file and its name\n");
